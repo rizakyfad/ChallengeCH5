6a9779df816de33513a6192a1c0a6c34aba4e52f
KinderJoy.ind <rizakifadhilla45@gmail.com>
2022-10-10 01:12:20 +0700

    Initial commit

---
+++ app/src/main/java/com/rizaki/challengech5/database/UserDao.kt
@@ -0,0 +1,19 @@
+package com.rizaki.challengech5.database
+
+import androidx.room.*
+
+
+@Dao
+interface UserDao {
+    @Query("SELECT *  From User where username = (:username) AND password =(:password)")
+    fun loginUser (username:String, password:String): List<User>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun Insertuser (user : User): Long
+
+    @Update
+    fun updateUser (user: User): Int
+
+    @Delete
+    fun deleteUser(user: User):Int
+}
---
+++ app/src/main/java/com/rizaki/challengech5/model/ListMovie.kt
@@ -0,0 +1,14 @@
+package com.rizaki.challengech5.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ListMovie(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Result>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
